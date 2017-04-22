b870a89ab49413e202f64876ec75dc30a4673874
Jeesang Kim <arsenalkim@gmail.com>
2017-04-22 21:05:31 +0900

    check to API-URL string used editbox

--- app/src/main/java/com/io/jees/blockchainapp/MainActivity.java
+++ app/src/main/java/com/io/jees/blockchainapp/MainActivity.java
@@ -1,13 +1,35 @@
 package com.io.jees.blockchainapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.io.jees.blockchainapp.parser.GsonParseBlockchain;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button.OnClickListener mClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            // start to parse Blockchain JSON
+            // get string for api-url
+            EditText hashText = (EditText) findViewById(R.id.et_hash);
+            String hash = hashText.getText().toString();
+
+            GsonParseBlockchain gpb = new GsonParseBlockchain();
+            gpb.parseSingleBlock(hash);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_search).setOnClickListener(mClickListener);
+
     }
 }
--- app/src/main/java/com/io/jees/blockchainapp/parser/GsonParseBlockchain.java
+++ app/src/main/java/com/io/jees/blockchainapp/parser/GsonParseBlockchain.java
@@ -1,5 +1,18 @@
 package com.io.jees.blockchainapp.parser;
 
-public class GsonParseBlockchain {
+import android.util.Log;
 
+public class GsonParseBlockchain {
+    public static final String TAG = "GSON";
+
+    public static String url_singleBlock = "https://blockchain.info/ko/rawblock/";                        // https://blockchain.info/ko/rawblock/$block_hash
+    public static String url_singleTransaction = "https://blockchain.info/ko/rawtx/";                     // https://blockchain.info/ko/rawtx/$tx_hash
+
+
+    public void parseSingleBlock(String hash) {
+        String url_api = url_singleBlock + hash;
+        Log.d(TAG, url_api);
+
+
+    }
 }
