6c648de399e4cc1d06d4c63634452d38bf6576b5
Jeesang Kim <arsenalkim@gmail.com>
2017-04-22 17:41:07 +0900

    new Blockchain model class

---
+++ app/src/main/java/com/io/jees/blockchainapp/model/Blockchain.java
@@ -0,0 +1,44 @@
+package com.io.jees.blockchainapp.model;
+
+import java.util.HashMap;
+
+public class Blockchain {
+
+    private HashMap<String, Integer> hash;
+    private HashMap<String, Integer> prev_block;
+    private int n_tx;
+    private int size;
+
+
+    public HashMap<String, Integer> getHash() {
+        return hash;
+    }
+
+    public void setHash(HashMap<String, Integer> hash) {
+        this.hash = hash;
+    }
+
+    public HashMap<String, Integer> getPrev_block() {
+        return prev_block;
+    }
+
+    public void setPrev_block(HashMap<String, Integer> prev_block) {
+        this.prev_block = prev_block;
+    }
+
+    public int getTxNumber() {
+        return n_tx;
+    }
+
+    public void setTxNumber(int n_tx) {
+        this.n_tx = n_tx;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+}
