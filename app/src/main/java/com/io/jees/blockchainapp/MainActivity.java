c726cba46edfe68debba99034e642ded0b70ec58
Jeesang Kim <arsenalkim@gmail.com>
2017-04-23 23:11:35 +0900

    Adjust to request switch for INPUT or OUTPUT

--- app/src/main/java/com/io/jees/blockchainapp/MainActivity.java
+++ app/src/main/java/com/io/jees/blockchainapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -38,8 +39,32 @@ public class MainActivity extends AppCompatActivity {
             EditText hashText = (EditText) findViewById(R.id.et_hash);
             String hash = hashText.getText().toString();
 
-            hash = "0000000000000bae09a7a393a8acded75aa67e46cb81f7acaa5ad94f9eacd103";
-            requestJsonObject(hash);
+            // Request and Parse from JSON
+            requestJsonObject(hash, NONE);
+        }
+    };
+
+    Button.OnClickListener mClickListenerInput = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // Get string of hash
+            EditText hashText = (EditText) findViewById(R.id.et_hash);
+            String hash = hashText.getText().toString();
+
+            // Request and Parse from JSON
+            requestJsonObject(hash, INPUT);
+        }
+    };
+
+    Button.OnClickListener mClickListenerOutput = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // Get string of hash
+            EditText hashText = (EditText) findViewById(R.id.et_hash);
+            String hash = hashText.getText().toString();
+
+            // Request and Parse from JSON
+            requestJsonObject(hash, OUTPUT);
         }
     };
 
@@ -49,9 +74,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         findViewById(R.id.btn_search).setOnClickListener(mClickListenerSearch);
+        findViewById(R.id.btn_input).setOnClickListener(mClickListenerInput);
+        findViewById(R.id.btn_output).setOnClickListener(mClickListenerOutput);
     }
 
-    private void requestJsonObject(String hash) {
+    private void requestJsonObject(String hash, final int resultType) {
         RequestQueue queue = Volley.newRequestQueue(this);
 
         String url = urlSingleBlock + hash;
@@ -59,21 +86,16 @@ public class MainActivity extends AppCompatActivity {
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
-                // Log.d(TAG, "Response " + response);
+                //Log.d(TAG, "Response " + response);
 
+                // JSON parsing used GSON
                 GsonBuilder builder = new GsonBuilder();
                 Gson mGson = builder.create();
+                List<BlockchainData> datas = new ArrayList<BlockchainData>();
+                datas = Arrays.asList(mGson.fromJson(response, BlockchainData.class));
 
-                BlockchainData bd = new BlockchainData();
-                List<BlockchainData> posts = new ArrayList<BlockchainData>();
-                posts = Arrays.asList(mGson.fromJson(response, bd.getClass()));
-
-                Log.d(TAG, "size " + posts.get(0).getSize());
-
-                /*
-                adapter = new RecyclerViewAdapter(MainActivity.this, posts);
-                recyclerView.setAdapter(adapter);
-                */
+                showResult(datas, resultType);
+                //Log.d(TAG, "size " + posts.get(0).getSize());
             }
         }, new Response.ErrorListener() {
             @Override
@@ -82,5 +104,41 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         queue.add(stringRequest);
+    }
+
+    private void showResult(List<BlockchainData> datas, int showType) {
+        int nTx = datas.get(0).getN_tx();
+        float avgTxValue = datas.get(0).getBits() / nTx;
+        float avgTxFee = datas.get(0).getFee() / nTx;
+        float avgTxSize = datas.get(0).getSize() / nTx;
+
+        long inSequence = datas.get(0).getTx().get(0).getInputs().get(0).getSequence();
+        int outTxIdx = datas.get(0).getTx().get(0).getOut().get(0).getTx_index();
+
+        String string;
+
+        switch (showType) {
+            case NONE:
+                string = "트랜젝션의 수 " + nTx +
+                        "\n평균 트랜젝션의 값 " + avgTxValue +
+                        "\n평균 트랜젝션의 수수료 " + avgTxFee +
+                        "\n평균 트랜젝션의 크기 " + avgTxSize;
+                break;
+
+            case INPUT:
+                string = "INPUT 의 Sequence " + inSequence;
+                break;
+
+            case OUTPUT:
+                string = "OUTPUT 의 트랜젝션 인덱스 " + outTxIdx;
+                break;
+
+            default:
+                string = "";
+                break;
+        }
+
+        TextView textView = (TextView) findViewById(R.id.tv_result);
+        textView.setText(string);
     }
 }
