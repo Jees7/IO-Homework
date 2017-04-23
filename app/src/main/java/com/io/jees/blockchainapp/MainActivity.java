d4e94432e3f321dbfc35f75f11553635faa45e3e
Jeesang Kim <arsenalkim@gmail.com>
2017-04-23 16:37:46 +0900

    Change to blockchain model class and then tested JSON parser with Volley
    Enhanced blockchain model class for list type. Add to Volley for string
    stream from JSON formatted.

--- app/src/main/java/com/io/jees/blockchainapp/MainActivity.java
+++ app/src/main/java/com/io/jees/blockchainapp/MainActivity.java
@@ -2,14 +2,30 @@ package com.io.jees.blockchainapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
-import com.io.jees.blockchainapp.parser.GsonBlockchain;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.io.jees.blockchainapp.model.BlockchainData;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
+
+    public  static final String urlSingleBlock = "https://blockchain.info/ko/rawblock/";                        // https://blockchain.info/ko/rawblock/$block_hash
+    public  static final String urlSingleTransaction = "https://blockchain.info/ko/rawtx/";                     // https://blockchain.info/ko/rawtx/$tx_hash
 
     Button.OnClickListener mClickListenerSearch = new View.OnClickListener() {
         @Override
@@ -19,16 +35,8 @@ public class MainActivity extends AppCompatActivity {
             EditText hashText = (EditText) findViewById(R.id.et_hash);
             String hash = hashText.getText().toString();
 
-            // search for blockchain information of hash
-            GsonBlockchain gb = new GsonBlockchain();
-            gb.parseSingleBlock(hash);
-
-            // show data on textview
-            TextView tv_ntx = (TextView) findViewById(R.id.tv_ntx);
-
-
-
-
+            hash = "0000000000000bae09a7a393a8acded75aa67e46cb81f7acaa5ad94f9eacd103";
+            requestJsonObject(hash);
         }
     };
 
@@ -38,9 +46,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         findViewById(R.id.btn_search).setOnClickListener(mClickListenerSearch);
+    }
 
+    private void requestJsonObject(String hash) {
+        RequestQueue queue = Volley.newRequestQueue(this);
 
+        String url = urlSingleBlock + hash;
 
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                // Log.d(TAG, "Response " + response);
 
+                GsonBuilder builder = new GsonBuilder();
+                Gson mGson = builder.create();
+
+                BlockchainData bd = new BlockchainData();
+                List<BlockchainData> posts = new ArrayList<BlockchainData>();
+                posts = Arrays.asList(mGson.fromJson(response, bd.getClass()));
+
+                Log.d(TAG, "size " + posts.get(0).getSize());
+
+                /*
+                adapter = new RecyclerViewAdapter(MainActivity.this, posts);
+                recyclerView.setAdapter(adapter);
+                */
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(TAG, "Error " + error.getMessage());
+            }
+        });
+        queue.add(stringRequest);
     }
 }
